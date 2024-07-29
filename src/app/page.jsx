18261da4422f99d53000c65788709eebaa8ca111
Berna Elmas <bernaelms1368@gmail.com>
2024-07-29 19:54:31 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <h1 className={styles.title}>Better design for your digital my products.</h1>
         <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry.</p>
         <Button url="/portfolio" text="See Our Works"/>
       </div>
