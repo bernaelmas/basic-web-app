c6d05724c939eb78ee4499f2f321b5997070374a
Berna Elmas <bernaelms1368@gmail.com>
2024-07-31 12:36:06 +0300

    made theme context

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
 
 const BlogPost = () => {
   return (
-    <div className={styles.container}>BlogPost</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}></h1>
+          <p className={styles.desc}></p>
+          <div className={styles.author}>
+            <Image
+            src="https://images.pexels.com/photos/3772623/pexels-photo-3772623.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={40}
+            height={40}
+            className={styles.avatar}
+            />
+            <span className={styles.username}>Jhon Don</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+          src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill={true}
+          className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}></p>
+      </div>
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,11 +5,11 @@ import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      <Link href="/testId" className={styles.container}>
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
        <div className={styles.imgContainer}>
         <Image 
-         src="" 
+         src="https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=600" 
          alt="" 
          width={400} 
          height={250} 
@@ -17,8 +17,38 @@ const Blog = () => {
         />
        </div>
        <div className={styles.content}>
-         <h1 className={styles.title}></h1>
-         <p className={styles.desc}></p>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
+       </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+       <div className={styles.imgContainer}>
+        <Image 
+         src="https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=600" 
+         alt="" 
+         width={400} 
+         height={250} 
+         className={styles.image}
+        />
+       </div>
+       <div className={styles.content}>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
+       </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+       <div className={styles.imgContainer}>
+        <Image 
+         src="https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=600" 
+         alt="" 
+         width={400} 
+         height={250} 
+         className={styles.image}
+        />
+       </div>
+       <div className={styles.content}>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
        </div>
       </Link>
     </div>
