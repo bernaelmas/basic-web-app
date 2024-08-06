25d0cf3bd36abeecdcfbbbd2a3ea8198ad2dbae1
Berna Elmas <bernaelms1368@gmail.com>
2024-08-06 15:03:42 +0300

    changed title

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from "./page.module.css"
 import Image from 'next/image';
 import Button from "@/components/Button/Button";
 
+export const metadata = {
+  title: "MammaMia Contact Information",
+  description: "This is Contact Information",
+};
+
 const Contact = () => {
   return (
     <div className={styles.container}>
